4eca21c0908062cac1a26bed1c83038d89eba483
SugiuraAyanoo <cyrustroy.c.bazar@isu.edu.ph>
2025-10-06 13:43:37 +0800

    Challenge.py: thats inch not a cm

--- midterm/mathopperations/Challenge.py
+++ midterm/mathopperations/Challenge.py
@@ -1,4 +1,4 @@
-#Challenge: write a short program that takes a number of cm as input and converts it to feet and inches
+#Challenge: write a short program that takes a number of inches as input and converts it to feet and inches
 
 # Hint: 1 foot = 12 inches. use // and %
 
@@ -6,6 +6,6 @@
 def Number(number : int):
     foot = number // 12
     inch = number % 12
-    print(f"{number} cm is: {foot} feet and {inch} inches.")
+    print(f"{number} inch in foot is: {foot} feet and {inch} inches.")
 
 Number(100)
